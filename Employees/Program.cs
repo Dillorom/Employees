130232676b8d56882adfc56a9403953fe9f0d967
Dillorom <37942024+Dillorom@users.noreply.github.com>
2019-01-29 10:34:19 -0800

    update the Sort and Find commands with categories to sort by and find by

--- Employees/Program.cs
+++ Employees/Program.cs
@@ -55,26 +55,175 @@ namespace Employees
                 }
                 else if (command == "Sort")
                 {
-                    List<Employee> sortedEmployees = employees.OrderBy(e => e.Name).ToList();
+                    Console.WriteLine("What category would you like to sort by? Enter one of the options: Name, DateOfBirth, Salary");
 
-                    foreach (Employee e in sortedEmployees)
+                    string category = Console.ReadLine();
+
+                    if (category == "Name")
                     {
-                        Console.WriteLine($"Name: {e.Name}, Date of Birth: {e.DateOfBirth}," +
-                                $" Salary: {e.Salary}");
+                        Console.WriteLine("Ascending or Descending?");
+
+                        string choice = Console.ReadLine();
+                        if (choice == "Ascending")
+                        {
+                            List<Employee> sortedEmployees = employees.OrderBy(e => e.Name).ToList();
+
+                            foreach (Employee e in sortedEmployees)
+                            {
+                                Console.WriteLine($"Name: {e.Name}, Date of Birth: {e.DateOfBirth}," +
+                                        $" Salary: {e.Salary}");
+                            };
+                        }
+                        else if (choice == "Descending")
+                        {
+                            List<Employee> sortedEmployees = employees.OrderBy(e => e.Name).Reverse().ToList();
+
+                            foreach (Employee e in sortedEmployees)
+                            {
+                                Console.WriteLine($"Name: {e.Name}, Date of Birth: {e.DateOfBirth}," +
+                                        $" Salary: {e.Salary}");
+                            };
+                        }
+                        else
+                        {
+                            Console.WriteLine("I do not understand this command. Please, enter 'Ascending' or 'Descending'.");
+                        }
+
+                    }
+                    else if (category == "DateOfBirth")
+                    {
+                        Console.WriteLine("Ascending or Descending?");
+
+                        string choice = Console.ReadLine();
+
+                        if (choice == "Ascending")
+                        {
+                            List<Employee> sortedEmployees = employees.OrderBy(e => e.DateOfBirth).ToList();
+
+                            foreach (Employee e in sortedEmployees)
+                            {
+                                Console.WriteLine($"Name: {e.Name}, Date of Birth: {e.DateOfBirth}," +
+                                        $" Salary: {e.Salary}");
+                            };
+                        }
+                        else if (choice == "Descending")
+                        {
+                            List<Employee> sortedEmployees = employees.OrderBy(e => e.DateOfBirth).Reverse().ToList();
+
+                            foreach (Employee e in sortedEmployees)
+                            {
+                                Console.WriteLine($"Name: {e.Name}, Date of Birth: {e.DateOfBirth}," +
+                                        $" Salary: {e.Salary}");
+                            };
+                        }
+                        {
+                            Console.WriteLine("I do not understand this command. Please, enter 'Ascending' or 'Descending'.");
+                        };
+                    }
+                    else if (category == "Salary")
+                    {
+                        Console.WriteLine("Ascending or Descending?");
+
+                        string choice = Console.ReadLine();
+
+                        if (choice == "Ascending")
+                        {
+                            List<Employee> sortedEmployees = employees.OrderBy(e => e.Salary).ToList();
+
+                            foreach (Employee e in sortedEmployees)
+                            {
+                                Console.WriteLine($"Name: {e.Name}, Date of Birth: {e.DateOfBirth}," +
+                                        $" Salary: {e.Salary}");
+                            };
+                        }
+                        else if (choice == "Descending")
+                        {
+                            List<Employee> sortedEmployees = employees.OrderBy(e => e.Salary).Reverse().ToList();
+
+                            foreach (Employee e in sortedEmployees)
+                            {
+                                Console.WriteLine($"Name: {e.Name}, Date of Birth: {e.DateOfBirth}," +
+                                        $" Salary: {e.Salary}");
+                            };
+                        }
+                        else
+                        {
+                            Console.WriteLine("I do not understand this command. Please, enter 'Ascending' or 'Descending'.");
+                        };
                     };
                 }
                 else if (command == "Find")
                 {
-                    Console.WriteLine("Enter the employee name.");
+                    Console.WriteLine("What category would you like to find by? Enter one of the options: Name, DateOfBirth, Salary");
 
-                    string name = Console.ReadLine();
+                    string category = Console.ReadLine();
 
-                    List<Employee> foundEmployees = employees.FindAll(e => e.Name == name);
-
-                    foreach (Employee e in foundEmployees)
+                    if (category == "Name")
                     {
-                        Console.WriteLine($" Name: {e.Name}, Date of Birth: {e.DateOfBirth}," +
-                            $" Salary: {e.Salary}");
+                        Console.WriteLine("Enter the employee name.");
+
+                        string enteredName = Console.ReadLine();
+
+                        List<Employee> foundEmployees = employees.FindAll(e => e.Name == enteredName);
+
+                        if (foundEmployees.Count != 0)
+                        {
+                            foreach (Employee e in foundEmployees)
+                            {
+                                Console.WriteLine($" Name: {e.Name}, Date of Birth: {e.DateOfBirth}," +
+                                    $" Salary: {e.Salary}");
+                            };
+                        }
+                        else
+                        {
+                            Console.WriteLine($"No employee by {enteredName} found in the list.");
+                        };
+                    }
+                    else if (category == "DateOfBirth")
+                    {
+                        Console.WriteLine("Enter the employee DateOfBirth.");
+
+                        string enteredDateOfBirth = Console.ReadLine();
+
+                        List<Employee> foundEmployees = employees.FindAll(e => e.DateOfBirth == enteredDateOfBirth);
+
+                        if (foundEmployees.Count != 0)
+                        {
+                            foreach (Employee e in foundEmployees)
+                            {
+                                Console.WriteLine($" Name: {e.Name}, Date of Birth: {e.DateOfBirth}," +
+                                    $" Salary: {e.Salary}");
+                            };
+                        }
+                        else
+                        {
+                            Console.WriteLine($"No employee by {enteredDateOfBirth} found in the list.");
+                        };
+                    }
+                    else if (category == "Salary")
+                    {
+                        Console.WriteLine("Enter the employee Salary.");
+
+                        int enteredSalary = int.Parse(Console.ReadLine());
+
+                        List<Employee> foundEmployees = employees.FindAll(e => e.Salary == enteredSalary);
+
+                        if (foundEmployees.Count != 0)
+                        {
+                            foreach (Employee e in foundEmployees)
+                            {
+                                Console.WriteLine($" Name: {e.Name}, Date of Birth: {e.DateOfBirth}," +
+                                    $" Salary: {e.Salary}");
+                            };
+                        }
+                        else
+                        {
+                            Console.WriteLine($"No employee by {enteredSalary} found in the list.");
+                        };
+                    }
+                    else
+                    {
+                        Console.WriteLine("I do not understand this category. Next time enter one of the options: Name, DateOfBirth, Salary");
                     };
                 }
                 else if (command == "Remove")
